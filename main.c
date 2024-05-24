a837c2c293d3444161b96310f8560a531ef1ebc3
Júlia Sales Novais <142419446+julsales@users.noreply.github.com>
2024-05-24 19:57:28 -0300

    tela inicial

--- main.c
+++ main.c
@@ -9,7 +9,39 @@
 int x = 34, y = 12;
 int incX = 1, incY = 1;
 
+int player1 = 0;
+int player2 = 0;
+
+void printbola(int nextX, int nextY);
+
+void printburaco(int x, int y, int raio);
+
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    static int ch = 0;
+    screenInit(20);
+    keyboardInit();
+    timerInit(50);
+    screenGotoxy(x,y);
+    screenSetColor(RED, DARKGRAY);
+    printf("Mini Golf");
+    screenSetColor(YELLOW, DARKGRAY);
+    screenGotoxy(25, 22);
+    printf("[SELECT ANY KEY TO CONTINUE]");
+    screenGotoxy(26, 23);
+    printf("                               ");
+    ch=getchar();
+}
+
+void printbola(int nextX, int nextY) {
+    screenSetColor(WHITE, DARKGRAY);
+    screenGotoxy(x, y);
+    printf(" ");
+    x = nextX;
+    y = nextY;
+    screenGotoxy(x, y);
+    printf("◌");
+}
+
+void printburaco(int x, int y, int raio){
+
 }
