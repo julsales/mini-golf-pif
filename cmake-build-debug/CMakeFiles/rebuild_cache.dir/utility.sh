d5d28fa40481ae17ba582803d62ffe623ac3a545
drakonbr <161368150+ChrisDrakon@users.noreply.github.com>
2024-05-26 15:04:19 -0300

    ball
    movimentação da bola feito com WASD

--- cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/julia/CLionProjects/MiniGolfe/cmake-build-debug
-/cygdrive/c/Users/julia/AppData/Local/JetBrains/CLion2024.1/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//mini golf
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,65 +6,64 @@
 #include "timer.h"
 
 int x = 34, y = 12;
-int incX = 1, incY = 1;
 
-int player1 = 0;
-int player2 = 0;
-
-struct buraco{
+struct buraco {
     int x;
     int y;
     int raio;
 };
 
 void menuinit();
-
 void printbola(int nextX, int nextY);
-
-void printburaco(struct buraco *bur,int x, int y, int raio);
-
+void printburaco(struct buraco *bur, int x, int y, int raio);
 void moverbola();
+void moveBall(char direction);
 
 int main(void) {
-    static int ch = 0;
+    int ch = 0;
     screenInit(20);
     keyboardInit();
     timerInit(60);
     menuinit();
+
     if (keyhit()) {
         ch = getchar();
         screenClear();
         screenInit(1);
         screenSetColor(WHITE, DARKGRAY);
-        screenGotoxy(x-4, y);
+        screenGotoxy(x - 4, y);
         printf("Selecione o modo de jogo");
-        screenGotoxy(x-4, y+1);
+        screenGotoxy(x - 4, y + 1);
         printf("Opção - 1");
-        screenGotoxy(x-4, y+2);
+        screenGotoxy(x - 4, y + 2);
         printf("Opção - 2");
+
         struct buraco *bur = (struct buraco *) malloc(2 * sizeof(struct buraco));
         ch = getchar();
-            switch (ch) {
-                case '1':
-                    screenInit(1);
-                    printbola(x, y);
-                    printburaco(&bur[0], 5, 10, 1);
-                    printburaco(&bur[1], 5, 20, 1);
-                    ch = getchar();
-                case '2':
-                    screenInit(1);
-                    printbola(x, y);
-                    printburaco(&bur[0], 5, 10, 1);
-                    printburaco(&bur[1], 5, 20, 1);
-                    ch = getchar();
-                default:
-                    screenGotoxy(x-4,y+3);
-                    printf("Digite uma opção válida");
-                    ch=getchar();
-            }
-            free(bur);
+
+        switch (ch) {
+            case '1':
+            case '2':
+                screenInit(1);
+                printbola(x, y);
+                printburaco(&bur[0], 5, 10, 1);
+                printburaco(&bur[1], 5, 20, 1);
+                while (1) {
+                    if (keyhit()) {
+                        ch = getchar();
+                        moveBall(ch);
+                    }
+                }
+                break;
+            default:
+                screenGotoxy(x - 4, y + 3);
+                printf("Digite uma opção válida");
+                ch = getchar();
         }
+        free(bur);
     }
+    return 0;
+}
 
 void printbola(int nextX, int nextY) {
     screenSetColor(WHITE, DARKGRAY);
@@ -78,28 +75,26 @@ void printbola(int nextX, int nextY) {
     printf("⬤");
 }
 
-void printburaco(struct buraco *bur,int x, int y, int raio) {
+void printburaco(struct buraco *bur, int x, int y, int raio) {
     screenSetColor(RED, DARKGRAY);
-    bur-> x = x;
-    bur -> y = y;
+    bur->x = x;
+    bur->y = y;
     bur->raio = raio;
-    int i;
-    int j;
-    for ( i = y - raio; i <= y + raio; i++) {
+    int i, j;
+    for (i = y - raio; i <= y + raio; i++) {
         for (j = x - raio; j <= x + raio; j++) {
             if ((x - j) * (x - j) + (y - i) * (y - i) <= raio * raio) {
                 screenGotoxy(j, i);
                 printf("-");
             }
-
         }
     }
-    screenGotoxy(j-2, i-4);
+    screenGotoxy(j - 2, i - 4);
     printf("⚐");
     screenSetColor(WHITE, DARKGRAY);
 }
 
-void menuinit(){
+void menuinit() {
     screenGotoxy(x, y);
     screenSetColor(RED, DARKGRAY);
     printf("MINI GOLF");
@@ -110,4 +105,15 @@ void menuinit(){
     screenGotoxy(26, 23);
     printf("                               ");
     screenSetNormal();
+}
+
+void moveBall(char direction) {
+    int nextX = x, nextY = y;
+    switch (direction) {
+        case 'w': nextY = y - 1; break;
+        case 'a': nextX = x - 1; break;
+        case 's': nextY = y + 1; break;
+        case 'd': nextX = x + 1; break;
+    }
+    printbola(nextX, nextY);
 }
